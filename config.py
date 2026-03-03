0e81a26f8af8f566ab14d9fc6300fe25b025bea9
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-03 09:47:52 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("23639272"))
-API_HASH = os.getenv("7d8f5d583048878a83071c5f267b059b")
+API_ID = int(os.getenv(""))
+API_HASH = os.getenv("")
 BOT_TOKEN = os.getenv("8345257530:AAFrl1ZjPy8gBvuJXVLedVPkUMksZjN6nik")
 
 FORCE_JOIN = os.getenv("FORCE_JOIN", "Anime_UpdatesAU")  # channel username without @
