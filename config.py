a864542e458ab95e62f28f8299883d8c1a30eca7
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-03 12:13:07 +0530

    config.py

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
 import os
 
-# Telegram API credentials
-API_ID = int(os.environ.get("API_ID", 0))
+API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-# Channel username WITHOUT @
 FORCE_JOIN = os.environ.get("FORCE_JOIN")
-
-# Your Telegram User ID
-OWNER_ID = int(os.environ.get("OWNER_ID", 0))
-
-# Web Port (Render auto sets this)
-PORT = int(os.environ.get("PORT", 8080))
+OWNER_ID = int(os.environ.get("OWNER_ID"))
