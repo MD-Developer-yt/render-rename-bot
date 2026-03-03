4318885273cc01008a9de028ebe8d97c46579150
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-03 18:04:22 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-FORCE_JOIN = os.environ.get("FORCE_JOIN")
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+PORT = int(os.environ.get("PORT", 8080))  # default 8080
