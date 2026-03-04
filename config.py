413132b2a2ee26f159ac194ae6b6932407156f2d
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-04 20:44:44 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-PORT = int(os.environ.get("PORT", 8080))  # default 8080
