81a9b03a3efc2907a0cd65214e063b3f7aa36714
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-05 15:29:54 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+import os
+
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "your_api_hash")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
+OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
